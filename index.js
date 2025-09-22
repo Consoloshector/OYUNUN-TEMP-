03b5ac204e4b29e2688e9db6d8118b337883e0be
Consoloshector <shiraliyev07@gmail.com>
2025-09-22 21:29:50 +0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const main = document.getElementsByClassName('main')[0];
                   link1: 'https://myfootball.life/11495535-marsel-pszh-pryamaya-translyaciya-franciya-liga-1-21092025-smotret-onlayn.html', 
                   link2: 'https://myfootball.life/11495535-marsel-pszh-pryamaya-translyaciya-franciya-liga-1-21092025-smotret-onlayn.html' },
                  
-                { oyun: 'Ballon Dor mərasimi', saat: '22:00', 
+                { oyun: 'Ballon Dor mərasimi', saat: '23:00', 
                 link1: 'https://myfootball.life/11496456-translyaciya-ceremonii-vrucheniya-zolotogo-myacha-2025.html',
                 link2: 'https://myfootball.life/11496456-translyaciya-ceremonii-vrucheniya-zolotogo-myacha-2025.html'},
 

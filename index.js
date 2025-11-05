c23cdf92f86900f46f409bc4347709feb93fbac3
Consoloshector <shiraliyev07@gmail.com>
2025-11-05 21:26:46 +0400

    Update match links and times in index.js

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 
         let oyunlar = [
                 { oyun: ' Qarabağ - Chelsea ', saat: '21:45', 
-                link1: 'https://www.youtube.com/live/gnWDSseIng8?si=NoJamt75lsiUJ7Zl', 
-                link2: 'https://myfootball.life/11505532-napoli-ayntraht-pryamaya-translyaciya-liga-chempionov-04112025-smotret-onlayn.html' },
+                link1: 'https://www.youtube.com/live/C8k3B5fut0s?si=7UotWKpbeGSO4G_h', 
+                link2: 'https://myfootball.life/11505558-karabah-chelsi-pryamaya-translyaciya-liga-chempionov-05112025-smotret-onlayn.html' },
 
-                { oyun: ' Ajax - Galatasaray ', saat: '21:45', 
-                  link1: 'https://golvar355.sbs/mac/slavia-prag-arsenal-tabii-spor/', 
-                  link2: 'https://myfootball.life/11505533-slaviya-praga-arsenal-pryamaya-translyaciya-liga-chempionov-04112025-smotret-onlayn.html' },
+                { oyun: ' Ajax - Galatasaray ', saat: '00:00', 
+                  link1: 'https://golvar395.sbs/mac/ajax-galatasaray-trt-1/', 
+                  link2: 'https://myfootball.life/11505565-ayaks-galatasaray-pryamaya-translyaciya-liga-chempionov-05112025-smotret-onlayn.html' },
                 
                  { oyun: ' Club Brugge - Barcelona ', saat: '00:00', 
-                 link1: 'https://golvar355.sbs/mac/juventus-sporting-lisbon-tabii-spor-2/', 
-                 link2: 'https://myfootball.life/11505545-yuventus-sporting-pryamaya-translyaciya-liga-chempionov-04112025-smotret-onlayn.html' },
+                 link1: 'https://golvar395.sbs/mac/club-brugge-barcelona-tabii-spor-1/', 
+                 link2: 'https://myfootball.life/11505568-bryugge-barselona-pryamaya-translyaciya-liga-chempionov-05112025-smotret-onlayn.html' },
                 
                 { oyun: ' Man City - Dortmund ', saat: '00:00', 
-                link1: 'https://golvar355.sbs/mac/liverpool-real-madrid-tabii-spor/',
-                link2: 'https://myfootball.life/11505546-liverpul-real-madrid-pryamaya-translyaciya-liga-chempionov-04112025-smotret-onlayn.html'},
+                link1: 'https://golvar395.sbs/mac/manchester-city-b-dortmund-tabii-spor/',
+                link2: 'https://myfootball.life/11505569-manchester-siti-borussiya-d-pryamaya-translyaciya-liga-chempionov-05112025-smotret-onlayn.html'},
 
                  /*{ oyun: ' PSG - Bayern  ', saat: '00:00', 
                    link1: 'https://golvar355.sbs/mac/psg-bayern-munih-tabii-spor-1/', 

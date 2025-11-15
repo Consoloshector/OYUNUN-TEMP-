1b7dad81d34f4d1c575b3aeace204c4d41a30752
Consoloshector <shiraliyev07@gmail.com>
2025-11-15 19:01:50 +0400

    Update game schedule and links in index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
    const main = document.getElementsByClassName('main')[0];
 
         let oyunlar = [
-                { oyun: ' Azərbaycan - İslandiya ', saat: '21:00', 
+                { oyun: ' Braziliya - Seneqal', saat: '20:00', 
                    link1: 'https://www.youtube.com/live/-itMdvajukg?si=Sl0du2JY1uCoCueK', 
                    link2: 'https://myfootball.life/11507538-azerbaydzhan-islandiya-pryamaya-translyaciya-otborochnyy-turnir-chm-evropa-13112025-smotret-onlayn.html' },
          
-                { oyun: ' İngiltərə - Serbiya ', saat: '23:45', 
+                { oyun: ' Türkiyə - Bolqarıstan ', saat: '21:00', 
                  link1: 'https://www.shoot-yalla.to/live/25806/1323347/england-vs-serbia', 
                  link2: 'https://myfootball.life/11507549-angliya-ang-serbiya-srb-pryamaya-translyaciya-otborochnyy-turnir-chm-evropa-13112025-smotret-onlayn.html' },
                 
-                { oyun: ' Moldova - İtaliya ', saat: '23:45', 
+                { oyun: ' Gürcüstan - İspaniya ', saat: '21:00', 
                 link1: 'https://www.shoot-yalla.to/live/25809/1366313/moldova-vs-italy',
                 link2: 'https://myfootball.life/11507546-moldaviya-mol-italiya-ita-pryamaya-translyaciya-otborochnyy-turnir-chm-evropa-13112025-smotret-onlayn.html'},
          
-                { oyun: ' İrlandiya - Portuqaliya ', saat: '23:45', 
+               /* { oyun: ' İrlandiya - Portuqaliya ', saat: '23:45', 
                 link1: 'https://www.shoot-yalla.to/live/25807/1366312/rep-of-ireland-vs-portugal', 
                 link2: 'https://myfootball.life/11507547-irlandiya-irl-portugaliya-por-pryamaya-translyaciya-otborochnyy-turnir-chm-evropa-13112025-smotret-onlayn.html' },
 
                 { oyun: ' Fransa - Ukraniya ', saat: '23:45', 
                   link1: 'https://www.youtube.com/watch?v=6lZmSgrvaSY', 
-                  link2: 'https://myfootball.life/11507548-franciya-fra-ukraina-ukr-pryamaya-translyaciya-otborochnyy-turnir-chm-evropa-13112025-smotret-onlayn.html' },
+                  link2: 'https://myfootball.life/11507548-franciya-fra-ukraina-ukr-pryamaya-translyaciya-otborochnyy-turnir-chm-evropa-13112025-smotret-onlayn.html' },*/
                 
                 /*{ oyun: ' Tottenham - Copenhagen  ', saat: '00:00', 
                  link1: 'https://golvar355.sbs/mac/tottenham-kopenhag-tabii-spor-4/', 

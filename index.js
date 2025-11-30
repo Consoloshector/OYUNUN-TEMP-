94c961b1c1c1de382681f4e95c7808d5aa1a1ec4
Consoloshector <shiraliyev07@gmail.com>
2025-11-30 17:49:21 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
    const main = document.getElementsByClassName('main')[0];
 
         let oyunlar = [
-                { oyun: ' Man City - Leeds United ', saat: '19:00', 
+                { oyun: ' Pisa - Inter ', saat: '18:00', 
                    link1: 'https://golvar279.sbs/mac/manchester-city-leeds-united-bein-sports-3/', 
                    link2: 'https://myfootball.life/11510716-manchester-siti-lids-yun-pryamaya-translyaciya-angliya-premer-liga-29112025-smotret-onlayn.html' },
          
-                { oyun: ' Barcelona - Deportivo ', saat: '19:15', 
+                { oyun: ' West Ham - Liverpool ', saat: '18:05', 
                  link1: 'https://golvar279.sbs/mac/barcelona-alaves-s-sport/', 
                  link2: 'https://myfootball.life/11510718-barselona-alaves-pryamaya-translyaciya-ispaniya-primera-divizion-29112025-smotret-onlayn.html' },
                 
-                { oyun: ' Monaco - PSG ', saat: '20:00', 
+                { oyun: ' Chelsea - Arsenal ', saat: '20:30', 
                 link1: 'https://golvar279.sbs/mac/monaco-psg-bein-sports-5/',
                 link2: 'https://myfootball.life/11510725-as-monako-pszh-pryamaya-translyaciya-franciya-liga-1-29112025-smotret-onlayn.html'},
          
-                { oyun: ' Bayer - Dortmund ', saat: '21:30', 
+                { oyun: ' Karagümrük - Beşiktaş ', saat: '21:00', 
                 link1: '#', 
                 link2: 'https://myfootball.life/11510734-bayer-l-borussiya-d-pryamaya-translyaciya-germaniya-bundesliga-29112025-smotret-onlayn.html' },
 
-               /* { oyun: ' PSG - Tottenham ', saat: '00:00', 
+                { oyun: ' Roma - Napoli ', saat: '23:45', 
                   link1: 'https://rotabettv371.com/mac/psg-tottenham-tabii-spor-3/', 
-                  link2: 'https://myfootball.life/11509893-pszh-tottenhem-pryamaya-translyaciya-liga-chempionov-26112025-smotret-onlayn.html' },*/
+                  link2: 'https://myfootball.life/11509893-pszh-tottenhem-pryamaya-translyaciya-liga-chempionov-26112025-smotret-onlayn.html' },
                 
-               /* { oyun: ' Napoli - Atalanta ', saat: '23:45', 
+                { oyun: ' Girona - Real Madrid ', saat: '00:00', 
                  link1: 'https://rotabettv370.com/mac/napoli-atalanta-s-sport-2/', 
-                 link2: 'https://myfootball.life/11508972-napoli-atalanta-pryamaya-translyaciya-italiya-seriya-a-22112025-smotret-onlayn.html' },*/
+                 link2: 'https://myfootball.life/11508972-napoli-atalanta-pryamaya-translyaciya-italiya-seriya-a-22112025-smotret-onlayn.html' },
 
                /* { oyun: ' Real Madrid - Valencia ', saat: '00:00', 
                  link1: 'https://rotabettv367.com/mac/real-madrid-valencia-s-sport/', 

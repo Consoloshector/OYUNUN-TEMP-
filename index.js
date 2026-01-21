34eacbf470550d2f828d053df13f3f72b1afb114
Consoloshector <shiraliyev07@gmail.com>
2026-01-21 20:28:35 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ let oyunlar = [
     saat: '21:45',
     links: [
       'https://www.canlitv.vin/cbcsport-canli-yayin',
-      'https://ovogoaal.com/match-updates/internazionale-vs-arsenal/',
-      'https://myfootball.pw/11522313-inter-arsenal-pryamaya-translyaciya-liga-chempionov-20012026-smotret-onlayn.html'
+      'https://myfootball.pw/11522318-karabah-ayntraht-pryamaya-translyaciya-liga-chempionov-21012026-smotret-onlayn.html',
     ]
   },
   {
@@ -15,27 +14,23 @@ let oyunlar = [
     saat: '21:45',
     links: [
       'https://www.canlitv.me/live/ictimaitv-canli-hd',
-      'https://ovogoaal.com/match-updates/real-madrid-vs-monaco/',
-      'https://rotabettv380.com/mac/real-madrid-monaco-tabii-spor-1/',
-      'https://myfootball.pw/11522314-real-madrid-as-monako-pryamaya-translyaciya-liga-chempionov-20012026-smotret-onlayn.html'
+      'https://myfootball.pw/11522317-galatasaray-atletiko-madrid-pryamaya-translyaciya-liga-chempionov-21012026-smotret-onlayn.html',
     ]
   },
   {
     oyun: ' Marseille - Liverpool ',
     saat: '00:00',
     links: [
-      'https://rotabettv380.com/mac/tottenham-b-dortmund-tabii-spor-3/',
-      'https://ovogoaal.com/match-updates/tottenham-hotspur-vs-borussia-dortmund/',
-      'https://myfootball.pw/11522312-tottenhem-borussiya-d-pryamaya-translyaciya-liga-chempionov-20012026-smotret-onlayn.html'
+      'https://www.canlitv.me/live/ictimaitv-canli-hd',
+      'https://myfootball.pw/11522325-slaviya-praga-barselona-pryamaya-translyaciya-liga-chempionov-21012026-smotret-onlayn.html'
     ]
   },
   {
     oyun: ' Slavia - Barcelona ',
     saat: '00:00',
     links: [
-      'https://rotabettv380.com/mac/sporting-lisbon-psg-tabii-spor-2/',
-      'https://ovogoaal.com/match-updates/sporting-cp-vs-psg/',
-      'https://myfootball.pw/11522311-sporting-pszh-pryamaya-translyaciya-liga-chempionov-20012026-smotret-onlayn.html'
+      'https://rotabettv380.com/mac/slavia-prag-barcelona-tabii-spor-2/',
+      'https://myfootball.pw/11522325-slaviya-praga-barselona-pryamaya-translyaciya-liga-chempionov-21012026-smotret-onlayn.html'
     ]
   },
   /*{
